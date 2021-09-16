8e70dea51e67a3304dee5a49625f8bbc5b785faf
Surendra <dasurendra@gmail.com>
2021-09-16 16:43:38 +1000

    server ready

--- server.js
+++ server.js
@@ -1,1 +1,26 @@
-console.log("dfngkjhnkjdfnjkgn");
+import express from 'express'
+const app = express()
+import cors from 'cors'
+import helmet from 'helmet'
+import morgan from 'morgan'
+const PORT = process.env.PORT || 8000
+
+//database
+
+//middleware
+app.use(helmet())
+app.use(cors())
+app.use(morgan('tiny'))
+
+//router
+app.use('/', (req, res) => {
+    res.send('you have reach the ned of the  router list')
+})
+
+app.listen(PORT, (error) => {
+    if (error) {
+        return console.log(error)
+    }
+
+    console.log(`Server is running at http://localhost:${PORT}`)
+})
